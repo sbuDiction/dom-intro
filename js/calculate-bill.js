c6e502b99de760f4ef4d55432af96c7fe8b8cd86
sbuDiction <sbudiction@gmail.com>
2019-05-21 12:30:52 +0200

    calculate bill

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,13 +1,33 @@
-//get a reference to the calculate button
+//references
+var calculateBtn = document.querySelector(".calculateBtn");
 
-//get a reference to the billTotal element
+var billTotalElement = document.querySelector(".billTotal");
 
-//get a reference to the billString
+var billStringField = document.querySelector(".billString");
 
-//create the function that will be called when the calculate button is pressed
-//  * this function should read the string value entered - split it on a comma.
-//  * loop over all the entries in the the resulting list
-//  * check if it is a call or an sms and add the right amount to the overall total
-//  * once done looping over all the entries - display the total onto the screen in the billTotal element
 
-//link the function to a click event on the calculate button
+//function
+function calculateBtnClicked(){
+    
+    var billString = billStringField.value;
+    
+    var billItems = billString.split(",");
+    
+    var billTotal = 0;
+    
+    for (var i=0;i<billItems.length;i++){
+        var billItem = billItems[i].trim();
+        if (billItem === "call"){
+            billTotal += 2.75;
+        }
+        else if (billItem === "sms"){
+            billTotal += 0.75;
+        }
+    }
+    
+
+    var roundedBillTotal = billTotal.toFixed(2);
+    billTotalElement.innerHTML = roundedBillTotal;
+}
+//adding evant
+calculateBtn.addEventListener('click', calculateBtnClicked);
